298a2c54bfdebaec550d900905225a192f8c0391
Fedail Yusein <fedailyusein@gmail.com>
2018-02-20 10:27:46 +0300

    update repos for oprganelle

--- src/organelle/src/index.ts
+++ src/organelle/src/index.ts
@@ -5,42 +5,63 @@ import * as euglena from "@euglena/core";
 import { sys, js } from "cessnalib";
 
 import Particle = euglena.AnyParticle;
-import Class = js.Class;
-import organelle = euglena_template.alive.organelle;
-import particles = euglena_template.alive.particle;
-import constants = euglena_template.alive.constants;
 
-let this_: Organelle = null;
-
-/**
- * Data model of the data which your organelle needed
- */
-export interface SapContent {
-    
-}
-
-export class Organelle extends euglena.alive.Organelle<SapContent> {
-    private sapContent: particles.DbOrganelleSapContent;
+export class Organelle extends euglena.alive.Organelle<particles.incoming.SapContent> {
+    public static readonly NAME = "CHANGE HERE WITH A UNIQUE NAME"
+    private sapContent: particles.incoming.SapContent;
     constructor() {
-        super(this_.name);
-        this_ = this;
+        super(Organelle.NAME);
     }
     protected bindActions(addAction: (particleName: string, action: (particle: Particle, callback: (particle: Particle) => void) => void) => void): void {
-        addAction(constants.particles.DbOrganelleSap, (particle: particles.DbOrganelleSap, callback) => {
-            this_.sapContent = particle.data;
-            this_.getAlive();
+        addAction(particles.incoming.Sap.NAME, (particle: particles.incoming.Sap, callback) => {
+            this.sapContent = particle.data;
+            this.getAlive();
         });
-        addAction(constants.particles.ReadParticle, (particle: particles.ReadParticle, callback) => {
-            let response = new euglena_template.alive.particle.Acknowledge(this.sapContent.euglenaName);
-            if (callback) {
-                callback(response);
-            } else {
-                this_.send(response);
-            }
-        });
+        /**
+         * TODO:
+         * Add Actions below in this method "bindActions" 
+         * 
+         */
     }
     private getAlive() {
-        this_.send(new euglena.ParticleV2(new euglena.MetaV2("NewOrganelleGotAlived",this.sapContent.euglenaName)));
+
+        /**
+         * TODO:
+         * Write something to make state of the organelle that
+         * organelle can take requests, and work.
+         */
+
+        /**
+         * send a notification to the Cytoplasm
+         * to inform about the organelle has been ready to get requests
+         * */
+        this.send(new euglena_template.alive.particle.OrganelleHasComeToLife(this.name,this.sapContent.euglenaName));
     }
 }
 
+export namespace particles {
+    export namespace incoming {
+        export interface SapContent {
+            euglenaName:string
+        }
+        export class Sap extends euglena.ParticleV2<SapContent>{
+            public static readonly NAME = Organelle.NAME+".sap";
+            
+            /**
+             *  TODO:
+             * Add fields needed from outside
+             * before started the organelle working
+             */
+
+            constructor(of:string,data:SapContent){
+                super(new euglena.MetaV2(Sap.NAME,of),data);
+            }
+        }
+    }
+    export namespace outgoing{
+
+    }
+    export namespace shared {
+        
+    }
+}
